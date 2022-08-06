be62330eef16d30d3f11eac26901fd2dcb6d8c9b
Lukas <lpovilaitis@yahoo.com>
2022-08-07 00:38:45 +0100

    redid how it makes divs to fix mirroring top row problem

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const container = document.getElementById('container')
 
 function makeTable() {
     
-    for(let rowIndex = 0; rowIndex < 16; rowIndex++) {
+    for(let rowIndex = 0; rowIndex < 256; rowIndex++) {
         let div = document.createElement('div');
        
-        for(let columnIndex = 0; columnIndex < 16; columnIndex++) {
-            let div1 = document.createElement('div');
-            div.appendChild(div1)
-            div1.classList.add('col');
-        }
+        // for(let columnIndex = 0; columnIndex < 16; columnIndex++) {
+        //     let div1 = document.createElement('div');
+        //     div.appendChild(div1)
+        //     div1.classList.add('col');
+        // }
         
         container.appendChild(div);
         div.classList.add('col', 'col1');
@@ -21,5 +21,5 @@ makeTable()
 let divs = document.querySelectorAll('.col');
 
 divs.forEach(divss => divss.addEventListener('mouseenter', function (e) {
-    e.target.style.backgroundColor = 'red'
+    e.target.style.backgroundColor = 'red';
 } ))
