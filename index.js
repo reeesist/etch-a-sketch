fe9e0991dfe18a2ee86cca9d044bdf7c9d8b31a6
Lukas <lpovilaitis@yahoo.com>
2022-08-07 14:51:17 +0100

    creates new square after asking size

--- index.js
+++ index.js
@@ -3,19 +3,11 @@ let box = 256
 function makeTable() {
     
     for(let rowIndex = 0; rowIndex < box; rowIndex++) {
-        let div = document.createElement('div');
-       
-        // for(let columnIndex = 0; columnIndex < 16; columnIndex++) {
-        //     let div1 = document.createElement('div');
-        //     div.appendChild(div1)
-        //     div1.classList.add('col');
-        // }
-        
+        let div = document.createElement('div');        
         container.appendChild(div);
         div.classList.add('col', 'col1');
     }
 }
-
 
 makeTable()
 let divs = document.querySelectorAll('.col');
@@ -23,18 +15,13 @@ let divs = document.querySelectorAll('.col');
 divs.forEach(divss => divss.addEventListener('mouseenter', function (e) {
     e.target.style.backgroundColor = 'blue';
 } ))
-
  
 function askSize() {
     size = prompt('Enter size of a square')
-    return box = size * size;
+    box = size * size;
     makeTable();
 }
 
-
 let btn = document.querySelector('button');
-btn.addEventListener('click', () => { 
-    askSize();
-
-})
+btn.addEventListener('click', askSize)
 
