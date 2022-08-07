d39d449c176d24f11d7959648e4fdeb2b8069151
Lukas <lpovilaitis@yahoo.com>
2022-08-07 19:51:32 +0100

    re-did how it makes rows and colums

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
-const container = document.getElementById('container')
-let box = 256
-function makeTable() {
-    
-    for(let rowIndex = 0; rowIndex < box; rowIndex++) {
-        let div = document.createElement('div');        
-        container.appendChild(div);
-        div.classList.add('col', 'col1');
-    }
+let container = document.querySelector('#container')
+container.style.gridTemplateColumns = `repeat(16, 1fr)`;
+container.style.gridTemplate.Rows = `repeat(16, 1fr)`;
+
+for(i = 0; i<256; i++) {
+    let square = document.createElement('div');
+    square.classList.add('square')
+    container.insertAdjacentElement("beforeend", square);
 }
 
-makeTable()
-let divs = document.querySelectorAll('.col');
-
+divs = document.querySelectorAll('.square');
 divs.forEach(divss => divss.addEventListener('mouseenter', function (e) {
     e.target.style.backgroundColor = 'blue';
 } ))
- 
-function askSize() {
-    size = prompt('Enter size of a square')
-    box = size * size;
-    makeTable();
-}
-
-let btn = document.querySelector('button');
-btn.addEventListener('click', askSize)
-
